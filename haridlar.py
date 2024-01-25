11ea3439434495254a8039a1dd19c8453583db8b
bahtiyormurotaliyev <147404784+bahtiyormurotaliyev@users.noreply.github.com>
2024-01-25 20:26:54 +0500

    first commit

---
+++ haridlar.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+belgilangan_kun = date(2024, 1, 25)  
+
+
+barcha_haridlar = [
+    "Harid 1",
+    "Harid 2",
+    "Harid 3",
+  
+]
+
+print(f"{belgilangan_kun} kuni:")
+for harid in barcha_haridlar:
+    print(harid)
+
+    
